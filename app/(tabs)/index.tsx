1e74d06f0df9a4c7a22d246d8f07a67877362f25
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2026-03-24 18:08:34 +0100

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,22 +8,13 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+            <Text className="text-3xl text-primary font-sans-extrabold">
+                Welcome to Recurrly!
             </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign up</Link>
 
-            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
-            <Link
-                href={{
-                    pathname: "/subscriptions/[id]",
-                    params: { id: "claude" },
-                }}
-            >
-                Claude Max Subscription
-            </Link>
+            <Link href="/onboarding" className="font-sans-bold mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
+            <Link href="/(auth)/sign-in" className=" font-sans-bold mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
+            <Link href="/(auth)/sign-up" className=" font-sans-bold mt-4 rounded bg-primary text-white p-4">Go to Sign up</Link>
         </SafeAreaView>
     );
 }
